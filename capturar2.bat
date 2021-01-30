dd2131609c13c942a1653999866545b0578e2ae4
moonlightz <34037569+moonlightz@users.noreply.github.com>
2021-01-30 16:09:53 +0000

    MEO-EPG

--- capturar2.bat
+++ capturar2.bat
@@ -1,4 +1,4 @@
-del meo.xml
-del channelinfo.xml
-DVBGuide.exe -aT -c474000 -d -e600  -fX -omeo
+;del meo.xml
+;del channelinfo.xml
+;DVBGuide.exe -aT -c474000 -d -e600  -fX -omeo
 tclsh -encoding utf-8 conversao.tcl
