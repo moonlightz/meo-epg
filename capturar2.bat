532ab1909539c4a1c20fb142938957b4198d5c19
moonlightz <34037569+moonlightz@users.noreply.github.com>
2021-02-12 21:19:59 +0000

    MEO-EPG

--- capturar2.bat
+++ capturar2.bat
@@ -1,4 +1,4 @@
-REM del meo.xml
-REM del channelinfo.xml
-REM DVBGuide.exe -aT -c474000 -d -e600  -fX -omeo
+del meo.xml
+del channelinfo.xml
+DVBGuide.exe -aT -c474000 -d -e600  -fX -omeo
 tclsh -encoding utf-8 conversao.tcl
