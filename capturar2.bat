d424c1556cc175891abc59b4d1c36763c17a61b2
Hugo <hugo.simoes.1984@gmail.com>
2024-08-18 11:35:26 +0100

    MEO-EPG

--- capturar2.bat
+++ capturar2.bat
@@ -2,5 +2,5 @@ del meo.xml
 del channelinfo.xml
 del meo.xml.gz
 del meo-modificado.xml.gz
-DVBGuide.exe -aT -c474000 -d -e600 -w -fC -omeo 
+DVBGuide.exe -aT -c474000 -d -e600 -w -fX -omeo 
 tclsh -encoding utf-8 conversao.tcl
