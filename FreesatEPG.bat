8c55fbb08882029ebb020881fab1b9640a53db89
Hugo <hugo.simoes.1984@gmail.com>
2024-08-20 21:52:59 +0100

    MEO-EPG

--- FreesatEPG.bat
+++ FreesatEPG.bat
@@ -1,5 +1,1 @@
-@echo off
-rem
-rem     Example batch file to capture the UK Freesat EPG as an XMLTV format file
-rem
-DVBGuide -as -c11427830 -ph -mQPSK -s27500000 -y3002 -z3003 -g c:\ChannelLogos -o freesat_epg.xml
+DVBGuide -as -c11427830 -ph -mQPSK -s27500000 -y3002 -z3003 -o freesat_epg.xml
--- MCGuideUpdate.bat
+++ MCGuideUpdate.bat
@@ -1,13 +1,13 @@
 @echo off
 rem
-rem     Example batch file to update the Media Center (MCE2005 or pre-TV Pack Vista) Guide from broadcast EPG
+rem     Example batch file to update the Media Center Guide from broadcast EPG
 rem
 rem     Notes:
 rem      - will normally need to be edited to specify tuning parameters etc.
 rem      - can be set to run as a scheduled task for completely automated guide updating
 rem      - (Vista only) must run as Administrator if using "-t" option to set system time
 rem
-DVBGuide -c505833 -t -fm -o mcguide.xml
+DVBGuide -c506000 -t -fm -o mcguide.xml
 if %ERRORLEVEL% neq 0 goto End
 MCGuideLoad mcguide.xml
 :End
