2e7e1e4d2d673cfd940966bce6ef53d490201474
Hugo <hugo.simoes.1984@gmail.com>
2024-08-22 00:29:30 +0100

    MEO-EPG

--- capturar2.bat
+++ capturar2.bat
@@ -1,7 +1,7 @@
 del meo.xml
-del channelinfo.xml
 del meo.xml.gz
 del meo-modificado.xml.gz
 REM DVBGuide.exe -aT -c474000 -d -e600 -w -fX -omeo 
-copy meo2.xml meo.xml
+REM tclsh -encoding utf-8 ott.tcl
+copy meo0.xml meo.xml
 tclsh -encoding utf-8 conversao.tcl
