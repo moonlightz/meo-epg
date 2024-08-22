efec1fc17394dbe06aac1eb0de1c49947e91f1dc
Hugo <hugo.simoes.1984@gmail.com>
2024-08-22 09:54:57 +0100

    MEO-EPG

--- capturar2.bat
+++ capturar2.bat
@@ -2,6 +2,5 @@ del meo.xml
 del meo.xml.gz
 del meo-modificado.xml.gz
 REM DVBGuide.exe -aT -c474000 -d -e600 -w -fX -omeo 
-REM tclsh -encoding utf-8 ott.tcl
-copy meo0.xml meo.xml
+tclsh -encoding utf-8 ott.tcl
 tclsh -encoding utf-8 conversao.tcl
--- ott.tcl
+++ ott.tcl
@@ -1,11 +1,14 @@
-###### 21 Agosto 2024
+###### 22-Agosto-2024
+
+set tempoinicial [clock milliseconds]
+
 puts "A iniciar a recolha pelo servidor da meo..."
 
 package require json
 
 #obter lista dos nomes dos canais
 
-set fp [open meo2.xml w+]
+set fp [open meo.xml w+]
 puts $fp "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
 puts $fp "<!DOCTYPE tv SYSTEM \"xmltv.dtd\">"
 puts $fp "<tv generator-info-name=\"MZ\" generator-info-url=\"git\" source-info-name=\"OTT MEO\" date=\"[clock format [clock seconds] -format "%Y%m%d%H%M%S %z"]\">"
@@ -47,7 +50,8 @@ foreach idcanal [lsort -increasing [array names canais]] {
 	set link "http://ott.online.meo.pt/Program/v8/Programs/LiveChannelPrograms?UserAgent=W10&\$orderby=StartDate%20asc&\$filter=CallLetter%20eq%20'$identificador'%20and%20StartDate%20ge%20datetime'[clock format [clock scan "1 day ago"] -format "%Y-%m-%d"]T01:00:00'%20and%20StartDate%20lt%20datetime'[clock format [clock scan "7 days"] -format "%Y-%m-%d"]T23:00:00'%20and%20IsEnabled%20eq%20true%20and%20IsAdultContent%20eq%20false%20and%20IsBlackout%20eq%20false"
 	set numprogs 0
 	set isymb 0
-	for {set a 0} {$a<100} {incr a} {
+	set tempoprog [clock seconds]
+	while {1} {
 		set cjson [exec wget -q -O - $link]
 		set cdict [json::json2dict $cjson]
 		set cvalue [dict get $cdict "value"]
@@ -66,7 +70,8 @@ foreach idcanal [lsort -increasing [array names canais]] {
 			puts $fp "    <category lang=\"pt\">$categoria</category>"
 			puts $fp "  </programme>"
 
-			puts -nonewline " [lindex "/ - \\\\ |" $isymb] [string replace [string repeat " " 40] 0 [string length $nomecanal]-1 $nomecanal]\r"
+			puts -nonewline [format " %1s %-10s  %-25s  %5s\r" [lindex "⌝ ⌟ ⌞ ⌜" $isymb] $identificador $nomecanal [clock format [expr [clock seconds]-$tempoprog] -format "%M:%S"]]
+
 			flush stdout
 			incr numprogs
 			incr isymb
@@ -74,12 +79,16 @@ foreach idcanal [lsort -increasing [array names canais]] {
 		}
 		if {[catch {set link [dict get $cdict "odata.nextLink"]} erro]} {
 			#Não há aquela chave, sair do loop
-			puts " v [string replace [string repeat " " 40] 0 [string length $nomecanal]-1 $nomecanal] Feito! $numprogs programas."
+			puts [format " %1s %-10s  %-25s  %5s  %-22s " "√" $identificador $nomecanal [clock format [expr [clock seconds]-$tempoprog] -format "%M:%S"] "Feito! $numprogs programas"]
+
 			break
 		}
 	}
-
+	#return
 
 }
 puts $fp "</tv>"
 close $fp
+
+puts "O tamanho do ficheiro meo.xml é [file size meo.xml] bytes."
+puts "A operação demorou [string map {"." ","} [expr ([clock milliseconds]-$tempoinicial)/1000.000]] segundos!"
